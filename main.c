3fa5dacf7fc56cc194a42d55fcd1d9a510050206
Waphie <siwaphiwehlazo@gmail.com>
2023-11-15 09:59:23 -0800

    Updated

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+int main()
+{
+	_printf( "standard output\n" );
+	printf("Siwa is a Twin");
+	printf("\________________\n");
+
+	printf("Homemade output\n");
+	_printf("Siwa is a Twin!\n");
+
+	return(0);
+}
